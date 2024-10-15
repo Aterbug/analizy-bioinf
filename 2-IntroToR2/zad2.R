d4cbbd36064c77e167dfa854967bff7957fa4297
Aterbug <mgstan2001@gmail.com>
2024-10-15 11:48:23 +0200

    Add files via upload
    zadanie 2

---
+++ 2-IntroToR2/zad2.R
@@ -0,0 +1,18 @@
+#ćw1
+#-----
+
+#library(ggplot2)
+
+#x<-15
+#y<-10
+
+#if x==y <-z
+#------
+#ćw2
+#-----
+library(ggplot2)
+library(plotly)
+
+# Pakiet 'plotly' służy do tworzenia interaktywnych wykresów.
+# Zainteresował mnie, ponieważ umożliwia tworzenie dynamicznych wizualizacji danych.
+###
